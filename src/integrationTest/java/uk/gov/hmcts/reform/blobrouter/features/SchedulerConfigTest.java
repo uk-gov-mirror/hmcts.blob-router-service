23b1da0450fb02a199a0a911655412141b1ca1e2
Donatas Martinkus <doncem@users.noreply.github.com>
2020-02-03 13:34:23 +0000

    Use `var` instead of explicit type definition

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/features/SchedulerConfigTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/features/SchedulerConfigTest.java
@@ -50,7 +50,7 @@ public class SchedulerConfigTest {
 
     @Test
     public void should_integrate_with_shedlock() throws Exception {
-        ArgumentCaptor<LockConfiguration> configCaptor = ArgumentCaptor.forClass(LockConfiguration.class);
+        var configCaptor = ArgumentCaptor.forClass(LockConfiguration.class);
 
         // wait for asynchronous run of the scheduled task in background
         Thread.sleep(2000);
